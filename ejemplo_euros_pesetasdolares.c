7d569826f6254f64f3f0367e017a0d956da622e4
Pauladeantoniogrondona <36919773+Pauladeantoniogrondona@users.noreply.github.com>
2018-03-23 09:52:45 +0100

    Fix char

--- ejemplo_euros_pesetasdolares.c
+++ ejemplo_euros_pesetasdolares.c
@@ -2,7 +2,8 @@
 int main()
 {
 	float euro, peseta, dolar;
-	printf ("Intraduzca la cantidad en euros\n");
+	// Usa printf
+	printf ("Introduzca la cantidad en euros\n");
 	scanf ("%f", &euro);
 	printf ("%f euros son %f pesetas y %f dolares\n", euro, euro/0.006, euro/1.23637);
 	return 0;
